92c50385b6cc87734988ce9640a013d56b23d52b
yzqi <yongqi014@gmail.com>
2021-09-11 21:42:02 -0700

    update toast

--- app/src/main/java/com/example/numad21fa_yongzhengqi/MainActivity.java
+++ app/src/main/java/com/example/numad21fa_yongzhengqi/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.numad21fa_yongzhengqi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void displayToast(View v) {
+        Toast.makeText(MainActivity.this, "Yongzheng Qi, qi.yo@northeastern.edu", Toast.LENGTH_LONG).show();
     }
 }
